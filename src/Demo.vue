f1d5d2691dd1a8002d974a55a7a55fcf83ac5653
yanqingqing <1585471322@qq.com>
2020-03-21 22:45:23 +0800

    创建vue项目

---
+++ src/Demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{n}}
+    <button @click="add()">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      n: 0
+    };
+  },
+  methods: {
+    add() {
+      this.n += 1;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+// console.log(window.Vue)
+// console.log(window.Vue)
+import Demo from './Demo.vue'
+console.log(Demo.render)
+// eslint-disable-next-line no-undef
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render(h) {
+        return h(Demo)
+
+    }
+    // data: {
+    //     n: 1
+    // },
+    // methods: {
+    //     add() {
+    //         this.n += 1;
+    //     }
+    // },
+    // //vue.runtime.min.js
+    // render(h) {
+    //     return h('div', [this.n, h('button', {
+    //         on: {
+    //             click: this.add
+    //         },
+
+    //     }, '+1')])
+    // }
+})
