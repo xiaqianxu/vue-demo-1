7bca8598d6e539cb3616c134d5e7fb1081ec41d9
yanqingqing <1585471322@qq.com>
2020-04-06 17:43:38 +0800

    不用computed筛选男女

--- src/main.js
+++ src/main.js
@@ -3,39 +3,59 @@ import Vue from "vue/dist/vue.js";
 
 Vue.config.productionTip = false;
 
+let id = 0;
+const createUser = (name, gender) => {
+    id += 1;
+    return {
+        id,
+        name,
+        gender
+    }
+}
 new Vue({
-    data: {
-        user: {
-            email: "fangyinghang@qq.com",
-            nickname: "方方1",
-            phone: "13812312312"
+    data() {
+        return {
+            users: [
+                createUser("方方", "男"),
+                createUser("晴晴", "女"),
+                createUser("楠楠", "女"),
+                createUser("长政", "男")
+            ],
+            displayUsers: []
         }
+    },
+    created() {
+        this.displayUsers = this.users
     },
     computed: {
-        displayName: {
-            get() {
-                const user = this.user
-                return user.nickname || user.email || user.phone
-            },
-            set(value) {
-                this.user.nickname = value
-            }
-        }
+
+
     },
+
     methods: {
-        add() {
-            this.displayName = "晴晴"
+        showMale() {
+            this.displayUsers = this.users.filter(u => u.gender === "男")
+        },
+        showFemale() {
+            this.displayUsers = this.users.filter(u => u.gender === "女")
+        },
+        showAll() {
+            this.displayUsers = this.users
         }
+
+
     },
 
     // DRY don't repeat yourself
     // 不如用 computed 来计算 displayName
-    template: `<div>
-    {{displayName}}
+    template: `
     <div>
-    {{displayName}}
-    <button @click="add">set</button>
-    </div>
-  </div>`,
+    <div><button @click="showAll">全部</button><button @click="showMale">男</button><button @click="showFemale">女</button></div>
+    <ul>
+        <li v-for="u in displayUsers" :key="u.id">
+        {{u.name}} - {{u.gender}}
+        </li>
+    </ul>
+    </div>`,
 
 }).$mount("#app");
