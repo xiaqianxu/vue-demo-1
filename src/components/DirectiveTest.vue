68a877e40e8f5437de64cf4992b6349b97a6d3a6
yanqingqing <1585471322@qq.com>
2020-04-07 22:38:10 +0800

    directive指定的使用

---
+++ src/components/DirectiveTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-focus />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DirectiveTest",
+  directives: {
+    focus: {
+      // 指令的定义
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // 引用完整版 Vue，方便讲解
 import Vue from "vue";
-import SyncTest from "@/components/SyncTest.vue"
+import DirectiveTest from "@/components/DirectiveTest.vue"
 
 Vue.config.productionTip = false;
 
@@ -14,98 +14,98 @@ Vue.config.productionTip = false;
 //     }
 // }
 new Vue({
-    render: h => h(SyncTest)
-//     data() {
-//         return {
-//             users: [
-//                 createUser("方方", "男"),
-//                 createUser("圆圆", "女"),
-//                 createUser("笑笑", "女"),
-//                 createUser("皮皮", "男")
-//             ],
-//             gender: '',
-//             n: 0,
-//             history: [],
-//             inUndoMode: false
-//         }
-//     },
+    render: h => h(DirectiveTest)
+    //     data() {
+    //         return {
+    //             users: [
+    //                 createUser("方方", "男"),
+    //                 createUser("圆圆", "女"),
+    //                 createUser("笑笑", "女"),
+    //                 createUser("皮皮", "男")
+    //             ],
+    //             gender: '',
+    //             n: 0,
+    //             history: [],
+    //             inUndoMode: false
+    //         }
+    //     },
 
-//     computed: {
-//         displayUsers() {
-//             console.log(this.gender, "被计算了一次")
-//             const hash = {
-//                 male: '男',
-//                 female: '女'
-//             };
-//             const {
-//                 users,
-//                 gender
-//             } = this;
-//             if (gender === "") {
-//                 return this.users;
-//             } else if (gender === "male" || gender === "female") {
-//                 return users.filter(u => u.gender === hash[gender])
-//             } else {
-//                 throw new Error("gender 不在列表中")
-//             }
+    //     computed: {
+    //         displayUsers() {
+    //             console.log(this.gender, "被计算了一次")
+    //             const hash = {
+    //                 male: '男',
+    //                 female: '女'
+    //             };
+    //             const {
+    //                 users,
+    //                 gender
+    //             } = this;
+    //             if (gender === "") {
+    //                 return this.users;
+    //             } else if (gender === "male" || gender === "female") {
+    //                 return users.filter(u => u.gender === hash[gender])
+    //             } else {
+    //                 throw new Error("gender 不在列表中")
+    //             }
 
-//         }
+    //         }
 
-//     },
-//     watch: {
-//         n: function (newValue, oldValue) {
-//             console.log(this.inUndoMode)
-//             if (!this.inUndoMode) {
-//                 this.history.push({
-//                     from: oldValue,
-//                     to: newValue
-//                 })
-//             }
-//         }
+    //     },
+    //     watch: {
+    //         n: function (newValue, oldValue) {
+    //             console.log(this.inUndoMode)
+    //             if (!this.inUndoMode) {
+    //                 this.history.push({
+    //                     from: oldValue,
+    //                     to: newValue
+    //                 })
+    //             }
+    //         }
 
-//     },
+    //     },
 
-//     methods: {
-//         setGender(String) {
-//             this.gender = String
-//         },
-//         add1() {
-//             this.n += 1;
-//         },
-//         add2() {
-//             this.n += 2;
-//         },
-//         minus1() {
-//             this.n -= 1;
-//         },
-//         minus2() {
-//             this.n -= 2;
-//         },
-//         undo() {
-//             const last = this.history.pop();
-//             this.inUndoMode = true;
-//             const old = last.from;
-//             this.n = old; //watch是异步的
-//             this.$nextTick(() => { //等到上边执行完之后才执行下面的操纵
-//                 this.inUndoMode = false
-//             })
-//         }
+    //     methods: {
+    //         setGender(String) {
+    //             this.gender = String
+    //         },
+    //         add1() {
+    //             this.n += 1;
+    //         },
+    //         add2() {
+    //             this.n += 2;
+    //         },
+    //         minus1() {
+    //             this.n -= 1;
+    //         },
+    //         minus2() {
+    //             this.n -= 2;
+    //         },
+    //         undo() {
+    //             const last = this.history.pop();
+    //             this.inUndoMode = true;
+    //             const old = last.from;
+    //             this.n = old; //watch是异步的
+    //             this.$nextTick(() => { //等到上边执行完之后才执行下面的操纵
+    //                 this.inUndoMode = false
+    //             })
+    //         }
 
-//     },
+    //     },
 
-//     // DRY don't repeat yourself
-//     // 不如用 computed 来计算 displayName
-//     template: `
-//     <div>
-//     <div><button @click="setGender('')">全部</button><button @click="setGender('male')">男</button><button @click="setGender('female')">女</button></div>
-//     <ul>
-//         <li v-for="u in displayUsers" :key="u.id">
-//         {{u.name}} - {{u.gender}}
-//         </li>
-//     </ul>
-  
-//     <hr/>
-    
+    //     // DRY don't repeat yourself
+    //     // 不如用 computed 来计算 displayName
+    //     template: `
+    //     <div>
+    //     <div><button @click="setGender('')">全部</button><button @click="setGender('male')">男</button><button @click="setGender('female')">女</button></div>
+    //     <ul>
+    //         <li v-for="u in displayUsers" :key="u.id">
+    //         {{u.name}} - {{u.gender}}
+    //         </li>
+    //     </ul>
+
+    //     <hr/>
+
     // {{n}}
     // <hr />
     // <button @click="add1">+1</button>
@@ -117,7 +117,7 @@ new Vue({
     // <hr/>
 
     // {{history}}
-//   </div>
-//     `,
+    //   </div>
+    //     `,
 
 }).$mount("#app");
